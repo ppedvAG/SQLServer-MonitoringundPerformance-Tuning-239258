f3e28bf98dd883420dc4a4c89702af47b88c8675
Rauchendraser <andreasr@ppedv.de>
2024-04-15 16:03:20 +0200

    Powerwoche
    Achtung : zwei Projekte  Teil1 und Teil 2

---
+++ SQL Monitoring Tuning/SQL Monitoring Tuning/Z_links.sql
@@ -0,0 +1,5 @@
+--TRaceflags +
+https://learn.microsoft.com/de-de/sql/t-sql/database-console-commands/dbcc-traceon-trace-flags-transact-sql?view=sql-server-ver16
+
+https://www.mssqltips.com/sqlservertip/4364/sql-server-2016-trace-flags-1118-and-1117-for-page-allocations/
+
